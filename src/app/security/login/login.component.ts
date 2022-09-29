8017c4149d98fe39ac2b0506521f256b9ec7d577
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2022-09-29 09:05:52 +0200

    update reg log, get data  form inputs on btn click

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+
+  loginGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  test(){
+    const email = this.loginGroup.value.email;
+    const password = this.loginGroup.value.password;
+    console.log(email, password);
+  }
 
   ngOnInit(): void {
   }
--- src/app/security/register/register.component.ts
+++ src/app/security/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
+
+  registerGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    repeatPassword: new FormControl('', Validators.required)
+  });
+
+  test(){
+    const email = this.registerGroup.value.email;
+    const password = this.registerGroup.value.password;
+    const repeatPassword = this.registerGroup.value.repeatPassword;
+    console.log(email, password, repeatPassword);
+  }
 
   ngOnInit(): void {
   }
