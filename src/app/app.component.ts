4552add756670ba9be265513a708d549d2991e36
TimotejkoPantak <timopantakgit@gmail.com>
2022-10-01 14:21:39 +0200

    Add login and reg buttons to navbar and make it responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginComponent } from './security/login/login.component';
 
 @Component({
   selector: 'app-root',
