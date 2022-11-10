b98c5a74931417da101cd601d830f6e46e4bc14c
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2022-11-10 09:04:25 +0100

    change routing

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -35,7 +35,7 @@ export class LoginComponent implements OnInit {
       const username = this.loginGroup.value.username;
       const password = this.loginGroup.value.password;
       this.authService.login(username, password)
-        .subscribe(() => this.router.navigateByUrl('/clicker'));
+        .subscribe(() => this.router.navigateByUrl('/boards'));
     }
   }
 
