5236672b22788e9cd7a12f7449722646f13b379f
JaroslavOndrus <jaroslavondrus04@gmail.com>
2022-12-12 23:11:51 +0100

    timetable changes

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { View } from '@syncfusion/ej2-angular-schedule';
+import { EventSettingsModel, RenderCellEventArgs, View } from '@syncfusion/ej2-angular-schedule';
+import { removeClass } from '@syncfusion/ej2-base';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
 @Component({
   selector: 'app-timetable',
-  template: '<ejs-schedule [currentView]="setView"></ejs-schedule>',
+  template: '<ejs-schedule [currentView]="setView" [showHeaderBar]="showHeaderBar" [eventSettings]="eventObject" [selectedDate]="setDate"></ejs-schedule>',
   //templateUrl: './timetable.component.html',
   styleUrls: ['./timetable.component.css']
 })
 export class TimetableComponent implements OnInit {
 
   public setView: View = 'WorkWeek';
+  public setDate: Date = new Date(2022, 11, 12);
+  private eventData: DataManager = new DataManager({
+    url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+    adaptor: new WebApiAdaptor,
+    crossDomain: true
+  });
+  public eventObject: EventSettingsModel = {
+    dataSource: this.eventData, 
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  
+  public showHeaderBar: Boolean = false;
 
 }
