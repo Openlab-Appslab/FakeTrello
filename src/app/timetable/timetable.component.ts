d6d306850f817367adde411820e54794ca44cca8
TimotejkoPantak <timopantakgit@gmail.com>
2022-12-20 09:11:56 +0100

    Add timetable grid and make lists of subjects

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -5,22 +5,24 @@ import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
 @Component({
   selector: 'app-timetable',
-  template: '<ejs-schedule [currentView]="setView" [showHeaderBar]="showHeaderBar" [eventSettings]="eventObject" [selectedDate]="setDate"></ejs-schedule>',
-  //templateUrl: './timetable.component.html',
+  // template: '<ejs-schedule [currentView]="setView" [showHeaderBar]="showHeaderBar" [eventSettings]="eventObject" [selectedDate]="setDate"></ejs-schedule>',
+  templateUrl: './timetable.component.html',
   styleUrls: ['./timetable.component.css']
 })
 export class TimetableComponent implements OnInit {
 
-  public setView: View = 'WorkWeek';
-  public setDate: Date = new Date(2022, 11, 12);
-  private eventData: DataManager = new DataManager({
-    url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
-    adaptor: new WebApiAdaptor,
-    crossDomain: true
-  });
-  public eventObject: EventSettingsModel = {
-    dataSource: this.eventData, 
-  }
+  // public setView: View = 'WorkWeek';
+  // public setDate: Date = new Date(2022, 11, 12);
+  // private eventData: DataManager = new DataManager({
+  //   url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+  //   adaptor: new WebApiAdaptor,
+  //   crossDomain: true
+  // });
+
+
+  // public eventObject: EventSettingsModel = {
+  //   dataSource: this.eventData, 
+  // }
 
   constructor() { }
 
@@ -29,4 +31,13 @@ export class TimetableComponent implements OnInit {
 
   public showHeaderBar: Boolean = false;
 
+  
+
 }
+
+let monday: string[] = ['', '', '', '', '', '', ''];
+let tuesday: string[] = ['', '', '', '', '', '', ''];
+let wednesday: string[] = ['', '', '', '', '', '', ''];
+let thursday: string[] = ['', '', '', '', '', '', ''];
+let friday: string[] = ['', '', '', '', '', '', ''];
+
